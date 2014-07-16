ff05d755cc019bab9df8a01fa022c35c69bac3e1
zhenqing.wang <zhenqing.wang@qunar.com>
2014-07-16 17:37:29 +0800

    add 题目1001：A+B for Matrices

--- C++/jobdu.cpp
+++ C++/jobdu.cpp
@@ -27,7 +27,7 @@ int callTime[maxn];
 int singleCallTime[maxn];
 int fatherArr[maxn];
 int visit[maxn];
- 
+
 struct Node{
     char name[4];
     int size;
@@ -209,7 +209,7 @@ int main(){
         for (int i = 0; i < len; i++) {
             for (int j = 0; j < len; j++) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = array[i][j]; 
+                    dp[i][j] = array[i][j];
                 }else if (i == 0) {
                     dp[i][j] = dp[i][j-1] + array[i][j];
                 }else if (j == 0) {
@@ -223,7 +223,7 @@ int main(){
     }
     return 0;
 }
- 
+
 /**************************************************************
     Problem: 1529
     User: wangzhenqing
@@ -231,4 +231,75 @@ int main(){
     Result: Accepted
     Time:0 ms
     Memory:1020 kb
+****************************************************************/
+
+// 题目1001：A+B for Matrices
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月16日17:32:32
+ * @url：http://ac.jobdu.com/problem.php?pid=1001
+ * 矩阵相加，求整行和整列为0的行列数。
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/37882211
+ */
+
+#include <stdio.h>
+const int max = 102;
+int matrix[max][max];
+int m, n;
+int i, j;
+int main(){
+    while(scanf("%d", &m) != EOF){
+        if(m == 0){
+            break;
+        }
+        scanf("%d", &n);
+        for(i = 0 ; i < m; i++){
+            for(j = 0; j < n; j++){
+                scanf("%d", &matrix[i][j]);
+            }
+        }
+
+        for(i = 0 ; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int tempNum;
+                scanf("%d", &tempNum);
+                matrix[i][j] += tempNum;
+            }
+        }
+        int zeroNum = 0;
+        for (i = 0; i < m; i++) {
+            bool isZero = true;
+            for (j = 0; j < n; j++) {
+                if (matrix[i][j] != 0) {
+                    isZero = false;
+                    break;
+                }
+            }
+            if (isZero) {
+                zeroNum++;
+            }
+        }
+        for (j = 0; j < n; j++) {
+            bool isZero = true;
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][j] != 0) {
+                    isZero = false;
+                    break;
+                }
+            }
+            if (isZero) {
+                zeroNum++;
+            }
+        }
+        printf("%d\n", zeroNum);
+    }
+    return 0;
+}
+/**************************************************************
+    Problem: 1001
+    User: wangzhenqing
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:1060 kb
 ****************************************************************/
--- Java/jobdu.java
+++ Java/jobdu.java
@@ -247,3 +247,81 @@ public class Main {
     Memory:14748 kb
 ****************************************************************/
 
+// 题目1001：A+B for Matrices
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月16日17:32:32
+ * @url：http://ac.jobdu.com/problem.php?pid=1001
+ * 矩阵相加，求整行和整列为0的行列数。
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/37882211
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+ 
+public class Main{
+    /*
+     * 1002 2014年07月16日16:20:45
+     */
+    public static void main(String[] args) throws Exception{
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(
+                    new InputStreamReader(System.in)));
+        while(st.nextToken() != StreamTokenizer.TT_EOF){
+            int m = (int)st.nval;
+            if(m == 0){
+                break;
+            }
+            st.nextToken();
+            int n = (int)st.nval;
+            int matrix[][] = new int[m][n];
+            for(int i = 0 ; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    st.nextToken();
+                    matrix[i][j] = (int)st.nval;
+                }
+            }
+ 
+            for(int i = 0 ; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    st.nextToken();
+                    matrix[i][j] += (int)st.nval;
+                }
+            }
+            int zeroNum = 0;
+            for (int i = 0; i < m; i++) {
+                boolean isZero = true;
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] != 0) {
+                        isZero = false;
+                        break;
+                    }
+                }
+                if (isZero) {
+                    zeroNum++;
+                }
+            }
+            for (int j = 0; j < n; j++) {
+                boolean isZero = true;
+                for (int i = 0; i < m; i++) {
+                    if (matrix[i][j] != 0) {
+                        isZero = false;
+                        break;
+                    }
+                }
+                if (isZero) {
+                    zeroNum++;
+                }
+            }
+            System.out.println(zeroNum);
+        }
+    }
+}
+/**************************************************************
+    Problem: 1001
+    User: wangzhenqing
+    Language: Java
+    Result: Accepted
+    Time:690 ms
+    Memory:14584 kb
+****************************************************************/

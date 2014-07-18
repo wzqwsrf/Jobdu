c74783b2f8000c29b34a6c87fa10c0449f6204b3
zhenqing.wang <zhenqing.wang@qunar.com>
2014-07-18 10:56:18 +0800

    add 1052,1054

--- C++/jobdu.cpp
+++ C++/jobdu.cpp
@@ -1089,3 +1089,85 @@ int main(){
     Memory:1020 kb
 ****************************************************************/
 
+
+
+//======================================
+// 题目1052：找x
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月18日10:35:29
+ * @url：http://ac.jobdu.com/problem.php?pid=1052
+ * 直接查找
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/37923401
+ */
+
+#include<stdio.h>
+int array[202];
+int findPos(int n,int x)
+{
+    int i=0;
+    for(i = 0; i < n; i++){
+        if(array[i] == x){
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    int i, x;
+    while(scanf("%d",&n) != EOF){
+        for(i = 0; i < n; i++){
+            scanf("%d",&array[i]);
+        }
+        scanf("%d",&x);
+        printf("%d\n",findPos(n,x));
+    }
+    return 0;
+}
+/**************************************************************
+    Problem: 1052
+    User: wangzhenqing
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:1020 kb
+****************************************************************/
+
+
+
+//======================================
+// 题目1054：字符串内排序
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月18日10:52:01
+ * @url：http://ac.jobdu.com/problem.php?pid=1054
+ * 内置排序方法可以直接实现
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/17221013
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <string.h>
+using namespace std;
+int main(){
+    char s[202];
+    while(scanf("%s",s) != EOF){
+      int len=strlen(s);
+      sort(s,s+len);
+      printf("%s\n",s);
+    }
+    return 0;
+}
+/**************************************************************
+    Problem: 1054
+    User: wangzhenqing
+    Language: C++
+    Result: Accepted
+    Time:0 ms
+    Memory:1020 kb
+****************************************************************/
+
--- Java/jobdu.java
+++ Java/jobdu.java
@@ -1090,3 +1090,99 @@ public class Main {
     Time:60 ms
     Memory:14552 kb
 ****************************************************************/
+
+
+
+//======================================
+// 题目1052：找x
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月18日10:35:29
+ * @url：http://ac.jobdu.com/problem.php?pid=1052
+ * 直接查找
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/37923401
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+ 
+public class Main{
+    /*
+     * 1052
+     */
+    public static void main(String[] args) throws Exception{
+        StreamTokenizer st = new StreamTokenizer(new BufferedReader(
+                    new InputStreamReader(System.in)));
+        while(st.nextToken() != StreamTokenizer.TT_EOF){
+            int n = (int)st.nval;
+            int array[] = new int[n];
+            for(int i = 0; i < n; i++){
+                st.nextToken();
+                array[i] = (int)st.nval;
+            }
+            st.nextToken();
+            int x = (int)st.nval;
+            System.out.println(findPos(array, x));
+        }
+    }
+    private static int findPos(int array[], int x){
+        int len = array.length;
+        for(int i = 0; i < len; i++){
+            if(array[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+/**************************************************************
+    Problem: 1052
+    User: wangzhenqing
+    Language: Java
+    Result: Accepted
+    Time:70 ms
+    Memory:14572 kb
+****************************************************************/
+
+
+
+
+//======================================
+// 题目1054：字符串内排序
+/* @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月18日10:52:01
+ * @url：http://ac.jobdu.com/problem.php?pid=1054
+ * 内置排序方法可以直接实现
+ * 解题思路参考http://blog.csdn.net/u013027996/article/details/17221013
+ */
+ 
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    /*
+     * 1054
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String a = scanner.nextLine();
+            char [] arrayA = a.toCharArray();
+            Arrays.sort(arrayA);
+            String result = "";
+            for (int i = 0; i < arrayA.length; i++) {
+                result += arrayA[i];
+            }
+            System.out.println(result);
+        }
+    }
+}
+/**************************************************************
+    Problem: 1054
+    User: wzqwsrf
+    Language: Java
+    Result: Accepted
+    Time:80 ms
+    Memory:15988 kb
+****************************************************************/

82c4f1bf052898f748a7f4c56dcec2c34b44a6b3
wangzhenqing <wzqwsrf@126.com>
2015-10-27 19:05:19 +0800

    fix 1468

--- Java/题目1468：Sharing.java
+++ Java/题目1468：Sharing.java
@@ -16,84 +16,85 @@ public class Main {
     /*
      * 1468
      */
-public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String array[] = new String[1000000];
-		Map<String, String> storeMap = new HashMap<String, String>();
-		while (scanner.hasNext()) {
-			String astart = scanner.next();
-			String bstart = scanner.next();
-			int num = scanner.nextInt();
-			for (int i = 0; i < num; i++) {
-				String firStr = scanner.next();
-				String letter = scanner.next();
-				String next = scanner.next();
-				storeMap.put(firStr, next);
-				array[Integer.parseInt(firStr)] = letter;
-			}
-			
-			String anext = astart;
-			String bnext = bstart;
-			int alen = 0;
-			while (!anext.equals("-1")) {
-				anext = storeMap.get(anext);
-				alen++;
-			}
-			
-			int blen = 0;
-			while (!bnext.equals("-1")) {
-				bnext = storeMap.get(bnext);
-				blen++;
-			}
-			anext = astart;
-			bnext = bstart;
-			if (alen == blen) {
-				while ( !anext.equals(bnext)) {
-					if (anext.equals("-1")) {
-						break;
-					}
-					anext = storeMap.get(anext);
-					bnext = storeMap.get(bnext);
-				}
-				System.out.println(anext);
-			}else if (alen > blen) {
-				int i = 0;
-				while (i < (alen - blen)) {
-					anext = storeMap.get(anext);
-					i++;
-				}
-				while ( !anext.equals(bnext)) {
-					if (anext.equals("-1")) {
-						break;
-					}
-					anext = storeMap.get(anext);
-					bnext = storeMap.get(bnext);
-				}
-				System.out.println(anext);
-			}else {
-				int i = 0;
-				while (i < (blen - alen)) {
-					bnext = storeMap.get(bnext);
-					i++;
-				}
-				while ( !anext.equals(bnext)) {
-					if (anext.equals("-1")) {
-						break;
-					}
-					anext = storeMap.get(anext);
-					bnext = storeMap.get(bnext);
-				}
-				System.out.println(anext);
-			}
-		}
-	}}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String array[] = new String[1000000];
+        Map<String, String> storeMap = new HashMap<String, String>();
+        while (scanner.hasNext()) {
+            String astart = scanner.next();
+            String bstart = scanner.next();
+            int num = scanner.nextInt();
+            for (int i = 0; i < num; i++) {
+                String firStr = scanner.next();
+                String letter = scanner.next();
+                String next = scanner.next();
+                storeMap.put(firStr, next);
+                array[Integer.parseInt(firStr)] = letter;
+            }
+
+            String anext = astart;
+            String bnext = bstart;
+            int alen = 0;
+            while (!anext.equals("-1")) {
+                anext = storeMap.get(anext);
+                alen++;
+            }
+
+            int blen = 0;
+            while (!bnext.equals("-1")) {
+                bnext = storeMap.get(bnext);
+                blen++;
+            }
+            anext = astart;
+            bnext = bstart;
+            if (alen == blen) {
+                while (!anext.equals(bnext)) {
+                    if (anext.equals("-1")) {
+                        break;
+                    }
+                    anext = storeMap.get(anext);
+                    bnext = storeMap.get(bnext);
+                }
+                System.out.println(anext);
+            } else if (alen > blen) {
+                int i = 0;
+                while (i < (alen - blen)) {
+                    anext = storeMap.get(anext);
+                    i++;
+                }
+                while (!anext.equals(bnext)) {
+                    if (anext.equals("-1")) {
+                        break;
+                    }
+                    anext = storeMap.get(anext);
+                    bnext = storeMap.get(bnext);
+                }
+                System.out.println(anext);
+            } else {
+                int i = 0;
+                while (i < (blen - alen)) {
+                    bnext = storeMap.get(bnext);
+                    i++;
+                }
+                while (!anext.equals(bnext)) {
+                    if (anext.equals("-1")) {
+                        break;
+                    }
+                    anext = storeMap.get(anext);
+                    bnext = storeMap.get(bnext);
+                }
+                System.out.println(anext);
+            }
+        }
+    }
+}
 /**************************************************************
- Problem: 1468
- User: wzqwsrf
- Language: Java
- Result: Accepted
- Time:2990 ms
- Memory:235160 kb
+ * Problem: 1468
+ * User: wzqwsrf
+ * Language: Java
+ * Result: Accepted
+ * Time:2990 ms
+ * Memory:235160 kb
  ****************************************************************/
 
 
